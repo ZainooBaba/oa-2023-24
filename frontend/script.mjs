8bcb182dcd3e92e9989beaa4c5427c140ea60407
ZainSiddi <135157583+ZainSiddi@users.noreply.github.com>
2023-09-17 22:07:42 -0500

    testing deployment

--- frontend/script.mjs
+++ frontend/script.mjs
@@ -8,20 +8,18 @@
 // 8. enter not working
 async function sendCoin() {
     //send post request to backend
-
-    addButtons(["bitcoin", "ethereum", "dogecoin"])
-
-    // const res = await fetch(
-    //     "http://localhost:3000/coinAdd:" + document.getElementById("nameField").value,
-    // )
-    // if(res.status == 200) {
-    //     addButtons(res.json())
-    // }
-    // console.log(res)
+    fetch(
+        "http://localhost:3000/coinAdd:" + document.getElementById("nameField").value,
+    ).then(res => res.json()).then( () => {
+        if(res.status == 200) {
+            addButtons(res.coins)
+        }
+        alert(res.json())
+    })
 }
 
 function addButtons(items) {
-    var allcoins=items.coins;
+    var allcoins=items;
         var buttonHolder=document.getElementById("coinSlect");
         buttonHolder.innerHTML="";
         for (var i = 0; i < allcoins.length; i++) {
